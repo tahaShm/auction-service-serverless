3b2f17ddbe32c9693867b20c101b7c5c4bd56d9d
tahashm <taha.shabani.m@gmail.com>
2024-12-11 20:03:36 -0800

    add dynamoDB write access in configuration

--- src/handlers/createAuction.mjs
+++ src/handlers/createAuction.mjs
@@ -1,12 +1,30 @@
+import { v4 as uuid } from "uuid";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb"; // ES Modules import
+// const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb"); // CommonJS import
+const client = new DynamoDBClient();
+
 async function createAuction(event, context) {
     const { title } = JSON.parse(event.body);
     const now = new Date();
 
     const auction = {
+        id: uuid(),
         title,
         status: "OPEN",
         createdAt: now.toISOString(),
     };
+
+    const command = new PutItemCommand({
+        TableName: "AuctionTable",
+        Item: {
+            id: { S: auction.id },
+            title: { S: auction.title },
+            status: { S: auction.status },
+            createdAt: { S: auction.createdAt },
+        },
+    });
+
+    await client.send(command);
 
     return {
         statusCode: 201, // resource created
