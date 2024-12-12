7d0652f68915a3db45e0bb12b46a9927df2a6622
tahashm <taha.shabani.m@gmail.com>
2024-12-11 22:25:34 -0800

    add middlewares for lambda functions

--- src/handlers/createAuction.mjs
+++ src/handlers/createAuction.mjs
@@ -1,10 +1,16 @@
 import { v4 as uuid } from "uuid";
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb"; // ES Modules import
 // const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb"); // CommonJS import
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
+
 const client = new DynamoDBClient();
 
 async function createAuction(event, context) {
-    const { title } = JSON.parse(event.body);
+    const { title } = event.body; // removed JSON.parse() as it's not needed with the httpJsonBodyParser() middleware
     const now = new Date();
 
     const auction = {
@@ -24,7 +30,12 @@ async function createAuction(event, context) {
         },
     });
 
-    await client.send(command);
+    try {
+        await client.send(command);
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error); // reduces the overhead of creating a new error for each type of error
+    }
 
     return {
         statusCode: 201, // resource created
@@ -32,4 +43,7 @@ async function createAuction(event, context) {
     };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+    .use(httpJsonBodyParser())
+    .use(httpEventNormalizer())
+    .use(httpErrorHandler());
