4ecd39ebc8b720781bb4e887a9c6086a2ec9743a
tahashm <taha.shabani.m@gmail.com>
2024-12-29 16:12:44 -0800

    forbid bidding on closed auctions

--- src/handlers/placeBid.mjs
+++ src/handlers/placeBid.mjs
@@ -13,6 +13,10 @@ async function placeBid(event, context) {
 
     const auction = await getAuctionById(id);
 
+    if (auction.status !== "OPEN") {
+        throw new createError.Forbidden("You can't bid on closed auctions!");
+    }
+
     if (amount <= auction.highestBid.amount) {
         throw new createError.Forbidden(
             `Your bid must be higher than ${auction.highestBid.amount}!`
